89521ae201de4008e0a21983a85732a89358779c
Alexander Winkowski <dereference23@outlook.com>
2023-05-21 07:17:38 +0000

    timekeep: Drop support for older SDK
    Change-Id: I2dcfede1a6e6007004d8a7ff6ca613b83dbc7ba6

--- Android.mk
+++ Android.mk
@@ -5,14 +5,9 @@ LOCAL_SRC_FILES := timekeep.c
 LOCAL_MODULE := timekeep
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -DANDROID_SDK_VERSION=$(PLATFORM_SDK_VERSION)
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 25),)
 LOCAL_MODULE_OWNER := sony
 LOCAL_INIT_RC      := vendor/etc/init/timekeep.rc
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 28),)
 LOCAL_PROPRIETARY_MODULE := true
-endif
-endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
